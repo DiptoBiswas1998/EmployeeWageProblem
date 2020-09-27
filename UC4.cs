1b4d0cc58b2d047ca94d77fbb5855db500b3ff76
Dipto <diptobiswas1998@gmail.com>
2020-09-27 15:17:29 +0530

    [Dipto] refactor: Using the switch case statement

---
+++ UC4.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome msg
+            Console.WriteLine("Welcome to the Employee Wage Problem!");
+            //Solution
+            int IsFullTime = 1;
+            int IsPartTime = 0;
+            int EmpRatePerHour = 20;
+
+            int empWage = 0;
+            int empHr = 8;
+            empWage = empHr * EmpRatePerHour;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case 0:
+                    Console.WriteLine("Part time Employee is present");
+                    Console.WriteLine("Part time Employee Wage = " + empWage);
+                    break;
+
+                case 1:
+                    Console.WriteLine("Full time Employee is present");
+                    Console.WriteLine("Full time Employee Wage = " + empWage);
+                    break;
+
+                default:
+                    Console.WriteLine("Neither Full Time nor Part time Employee is present!");
+                    break;
+            }
+        }
+    }
+}
