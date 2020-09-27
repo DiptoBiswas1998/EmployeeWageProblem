79e9c3e336a038fb67404c9f4a3258f4cef3755e
Dipto <diptobiswas1998@gmail.com>
2020-09-27 13:41:37 +0530

    [Dipto] refactor: I made changes

---
+++ UC1.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome msg
+            Console.WriteLine("Welcome to the Employee Wage Problem!");
+            //Check
+            int IsFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+        }
+    }
+}
