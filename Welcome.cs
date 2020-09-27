23f0bb1f789a1f6fd5a72901f6ec45cacc3932c5
Dipto <diptobiswas1998@gmail.com>
2020-09-27 13:11:04 +0530

    [Dipto] refactor: I adding the Welcome msg

---
+++ Welcome.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Problem!");
+        }
+    }
+}
